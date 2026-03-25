03e7789d905c69d9de449dee91a745e710935af5
mertcvk <253686188+mertcvk@users.noreply.github.com>
2026-03-25 12:51:49 +0200

    Create the logic to play a single round and decide on the winner based on the choices. Create the function to increase the score based on wins.

--- javascript.js
+++ javascript.js
@@ -11,15 +11,41 @@ if (computerChoice <= 33) {
     return ("Scissors")
 }
 }
-console.log(getComputerChoice())
 
 //Write the logic to get the human choice
 function getHumanChoice () {
     let humanChoice = prompt("Enter your choice:");
     return (humanChoice);
 }
-console.log(getHumanChoice())
 
 //Declare the players score variables
 let humanScore = 0
 let computerScore = 0
+
+//Write the logic to play a single round
+function playRound(humanChoice, computerChoice) { 
+    humanChoice = humanChoice.charAt(0).toUpperCase() + humanChoice.slice(1).toLowerCase() ;
+    if (humanChoice === "Rock" && computerChoice === "Rock") {
+        return ("Rock against Rock. It is a tie!") ; 
+    } else if (humanChoice === "Rock" && computerChoice === "Scissors") {
+        humanScore++ ;
+        return ("Rock against Scissors. It is a win!") ;
+    } else if (humanChoice === "Paper" && computerChoice === "Paper") {
+        return ("Paper against Paper. It is a tie!") ; 
+    } else if (humanChoice === "Paper" && computerChoice === "Rock") {
+        humanScore++ ;
+        return ("Paper against Rock. It is a win!") ;
+    } else if (humanChoice === "Scissors" && computerChoice === "Scissors") {
+        return ("Scissors against Scissors. It is a tie!") ; 
+    } else if (humanChoice === "Scissors" && computerChoice === "Paper") {
+       humanScore++ ;
+        return ("Scissors against Paper. It is a win!") ;
+    } else {
+        computerScore++ ;
+        return (humanChoice + " against " + computerChoice + "." + " It is a lose!")
+    }
+    }
+
+console.log(playRound(getHumanChoice(), getComputerChoice()));
+let result = ("You: " + humanScore + " & " + "Computer: " + computerScore)
+console.log(result)
